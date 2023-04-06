89190f3edf9c9980433fc763c1fd91abcd30af7b
HunterX405 <43195082+HunterX405@users.noreply.github.com>
2023-04-06 22:25:58 +0800

    update

--- src/pages/api/auth/callback/twitter.ts
+++ src/pages/api/auth/callback/twitter.ts
@@ -1,19 +1,9 @@
 import NextAuth from "next-auth"
-import TwitterProvider from "next-auth/providers/twitter"
-
-const options = {
-  providers: [
-    TwitterProvider({
-        clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID!,
-        clientSecret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET!
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "../[...nextauth]"
 
 export default async function callback(req: any, res: any) {
     try {
-      await NextAuth(req, res, options);
+      await NextAuth(req, res, authOptions);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal server error" })

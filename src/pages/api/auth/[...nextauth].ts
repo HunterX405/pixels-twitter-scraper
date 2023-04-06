022d96413b6c9a4133258afdf92936d1c55e7134
HunterX405 <43195082+HunterX405@users.noreply.github.com>
2023-04-06 16:13:52 +0800

    using next-auth/react

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import TwitterProvider from "next-auth/providers/twitter"
+
+export default NextAuth({
+    providers: [
+        TwitterProvider({
+            clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID!,
+            clientSecret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET!
+        }),
+    ],
+    callbacks: {
+        session({ session, token, user }) {
+          return session // The return type will match the one returned in `useSession()`
+        },
+    },
+});

ea390644a09b0e965aa1ed1ceb873c472c32e9b4
HunterX405 <43195082+HunterX405@users.noreply.github.com>
2023-04-06 21:34:14 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, getSession } from "next-auth/react";
+import { ReactElement } from 'react';
+import { GetServerSidePropsContext } from 'next';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+interface MyAppProps {
+  Component: React.ComponentType<any>;
+  pageProps: any;
+}
+
+function MyApp({ Component, pageProps }: MyAppProps): ReactElement {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
+}
+
+export default MyApp;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session: session ? {
+        user: {
+          name: session.user?.name,
+          email: session.user?.email,
+          image: session.user?.image,
+        },
+        expires: session.expires,
+      } : null,
+      // Add any other props you want to pass to your components here.
+    },
+  };
 }

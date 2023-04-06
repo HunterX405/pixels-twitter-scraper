4f70be966325811a963550e974d954354298bd8a
HunterX405 <43195082+HunterX405@users.noreply.github.com>
2023-04-06 22:40:24 +0800

    update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import TwitterProvider from "next-auth/providers/twitter"
-import NextAuth, { NextAuthOptions, Session, User } from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
@@ -15,10 +15,29 @@ export const authOptions: NextAuthOptions = {
         colorScheme: "light",
     },
     callbacks: {
-        async jwt({ token }) {
-            token.userRole = "admin"
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+              token.accessToken = account.access_token
+            }
             return token
-        },
+          },
+          async session({ session, token, user }) {
+            // Send properties to the client, like an access_token and user id from a provider.
+
+            return session
+          },
+        async signIn({ user, account, profile, email, credentials }) {
+            const isAllowedToSignIn = true
+            if (isAllowedToSignIn) {
+              return true
+            } else {
+              // Return false to display a default error message
+              return false
+              // Or you can return a URL to redirect to:
+              // return '/unauthorized'
+            }
+          }
     },
 }
 
